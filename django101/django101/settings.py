74c41a5ebc0fb012444cd1a7def8c8052ff3c328
Stefan Dimitrov <54206891+StefanDimitrovDimitrov@users.noreply.github.com>
2021-05-05 00:30:44 +0300

    Django Templetes

--- django101/django101/settings.py
+++ django101/django101/settings.py
@@ -123,10 +123,14 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = ''
-STATICFILES_DIRS = (
-    join(BASE_DIR, 'static'),
-)
+STATICFILES_DIRS = [
+    BASE_DIR/"static",
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# STATICFILES_DIRS = (
+#     join(BASE_DIR, 'static'),
+# )
--- django101/django101/urls.py
+++ django101/django101/urls.py
@@ -1,8 +1,5 @@
 from django.urls import path, include
 
-import django102
-from django102.views import index, UserListView, GamesListView
-
 urlpatterns = [
     path('', include('django102.urls'))
 ]
