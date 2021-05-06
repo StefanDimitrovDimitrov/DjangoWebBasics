57c8b4aa28f1c2879ee69b41982f6634e6363caa
Stefan Dimitrov <54206891+StefanDimitrovDimitrov@users.noreply.github.com>
2021-05-06 13:20:49 +0300

    Models

--- django101/django102/models.py
+++ django101/django102/models.py
@@ -2,4 +2,5 @@ from django.db import models
 
 
 class Game(models.Model):
-    name = models.CharField(max_length=30)
+    name = models.CharField(max_length=30, blank=False, default='')
+    level_of_difficylty = models.IntegerField(blank=False)
