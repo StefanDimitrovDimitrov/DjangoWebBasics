5616b308c4cf94f868957b3c46eb8a1ad9c4c7ca
Stefan Dimitrov <54206891+StefanDimitrovDimitrov@users.noreply.github.com>
2021-05-10 12:49:12 +0300

    Django models, POST and GET

--- django101/todos_app/forms.py
+++ django101/todos_app/forms.py
@@ -2,8 +2,53 @@ from django import forms
 
 
 class TodoForm(forms.Form):
-    title = forms.CharField(max_length=30)
-    description = forms.CharField(
-        widget=forms.Textarea()
+    title = forms.CharField(
+        max_length=30,
+        widget=forms.TextInput(
+            attrs={
+                'class': 'neshto-si',
+            }
+        )
     )
-    age = forms.IntegerField()
+
+    description = forms.CharField()
+
+    bot_catcher = forms.CharField(
+        widget=forms.HiddenInput,
+        required=False,
+    )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for (_, field) in self.fields.items():
+            if 'class' in field.widget.attrs:
+                value = field.widget.attrs['class'] + ' form-control'
+            else:
+                value = 'form-control'
+            field.widget.attrs['class'] = value
+
+    def clean_bot_catcher(self):
+        if len(self.cleaned_data['bot_catcher']):
+            raise forms.ValidationError('This is a bot')
+# password=forms.CharField(
+    #     widget=forms.PasswordInput(
+    #         attrs={
+    #             'class': 'form-control',
+    #         }
+    #     ),
+    # )
+    # age = forms.IntegerField(
+    #     widget=forms.NumberInput(
+    #         attrs={
+    #             'class': 'form-control',
+    #         }
+    #     ),
+    # )
+    # size = forms.IntegerField(
+    #     widget=forms.TextInput(
+    #         attrs={
+    #             'type': 'range',
+    #             'class': 'form-control class control'
+    #         },
+    #     ),
+    # )
